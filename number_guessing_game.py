83651d3b1257f6f1bedb1090c4c919de25d7f386
Linko <maihan.ahmeD@hotmail.co.uk>
2024-09-13 00:56:16 +0100

    Beginning of project

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -5,3 +5,8 @@
 # number of guesses will be displayed each guess and will be asked to input another guess
 # when user guesses the 'random_num' correctly a congratulations message will be displayed as well as the total number of guesses
 
+print("Welcome to the guessing game! Please input a range when asked and the machine will pick a number at random and you will guess the correct number with feedback displayed on whether your guess was too low or too high and will track the number of guesses. Good luck!")
+A = int(input(print("Please choose the starting number of the range")))
+B = int(input(print("Please choose the ending number of the range")))
+print(f"You have picked the range {A}-{B}")
+print(input("Please enter your first guess"))
