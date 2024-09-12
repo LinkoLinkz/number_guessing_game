c4a7e934417e860a6572208336c3ec1dd99dcd94
Linko <maihan.ahmeD@hotmail.co.uk>
2024-09-13 00:45:21 +0100

    Description of process flow

---
+++ number_guessing_game.py
@@ -0,0 +1,6 @@
+# user inputs a range A, B
+# machine will pick a random number within range(A, B) and store as the variable 'random_num'
+# user makes their first guess within within range(A, B) and is stored as the variable 'guess' (if outside then an error message is displayed)
+# user will be displayed feedback based on their input guess whether it is higher or lower than 'random_num'
+# number of guesses will be displayed each guess and will be asked to input another guess
+# when user guesses the 'random_num' correctly a congratulations message will be displayed as well as the total number of guesses
